b7815d5428afcab514ff2c5046ad89f836f4ad52
Tyler Glowaski <glowaski@Tylers-MacBook-Pro-5.local>
2020-04-07 15:39:55 -0400

    url connection lab

--- Lab04/app/src/main/java/com/tglowaski/lab04/MainActivity.java
+++ Lab04/app/src/main/java/com/tglowaski/lab04/MainActivity.java
@@ -3,6 +3,13 @@ package com.tglowaski.lab04;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.util.Log;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.net.MalformedURLException;
+import java.net.URL;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +17,18 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        try {
+            jsonString("https://jsonplaceholder.typicode.com/users");
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    private void jsonString(String url) throws IOException {
+        URL link = new URL(url);
+        link.openConnection();
+        BufferedReader in = new BufferedReader(new InputStreamReader(System.in));
+        String output = in.readLine();
+        Log.i(null,output);
     }
 }
