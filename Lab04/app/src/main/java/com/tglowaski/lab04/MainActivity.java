4b7c0e30f2814c04325cdedc4744735b84f6f2d7
Tyler Glowaski <glowaski@Tylers-MacBook-Pro-5.local>
2020-04-07 15:08:49 -0400

    webview lab

--- Lab04/app/src/main/java/com/tglowaski/lab04/MainActivity.java
+++ Lab04/app/src/main/java/com/tglowaski/lab04/MainActivity.java
@@ -3,6 +3,7 @@ package com.tglowaski.lab04;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.webkit.WebView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +11,9 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        WebView webview = new WebView(this);
+        setContentView(webview);
+        webview.loadUrl("https://www.youtube.com");
+
     }
 }
